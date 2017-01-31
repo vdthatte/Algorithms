caf4748d091af2a15237dc97293c9afd1fbf7e31
Vidyadhar Thatte <vd_thatte@yahoo.com>
2017-01-31 11:17:15 -0500

    added comments

--- divide.py
+++ divide.py
@@ -1,16 +1,29 @@
 
 def divide(x,y):
+	"""
+
+Using Recursion
+
+	"""
+
+	# Base Case
 	if x==0:
 		return (0,0)
 
+	# Recursion
 	(q,r) = divide((x/2),y)
+	
 	q = (2*q)
 	r = (2*r)
+	
+	# if x is odd add one to the remainder
 	if ((x%2) == 1):
 		r = r + 1
+	
+	# if remainder is greater than y, add one to the quotient and subtract y from remainder
 	if (r >= y):
-		r = r - y
-		q = q + 1
+		r = r - y 
+		q = q + 1 
 
 	return (q,r)
 
