7af9a9d6a3b4e9dd29e718a92a8bb4ba5a3ce12a
Vidyadhar Thatte <vd_thatte@yahoo.com>
2017-01-31 11:18:29 -0500

    added comments to multiply

--- multiply.py
+++ multiply.py
@@ -1,11 +1,19 @@
 
 
 def multiply(x,y):
+
+	# Base Case
 	if y==0:
 		return 0
+
+	# Recursion
 	z = multiply(x,y/2)
+
+	# If y is even, return two times y
 	if (y%2 == 0):
 		return (2*x)
+	
+	# If y is not even, return the following below
 	else:
 		return (x+(2*z))
 
